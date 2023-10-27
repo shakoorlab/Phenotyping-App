36d44a715492bc94217879b35476e134f1b4e9d1
JaccobStanton <jstanton23@my.stlcc.edu>
2023-10-27 15:39:45 -0500

    selection button complete on weather page

--- Phenotyping/screens/weather/WeatherLanding.js
+++ Phenotyping/screens/weather/WeatherLanding.js
@@ -1,10 +1,98 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { theme } from "./theme";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { MapPinIcon } from "react-native-heroicons/solid";
 
 const WeatherLanding = () => {
+  //--------------------- logic for search bar --------------------------
+  const [showSearch, toggleSearch] = useState(false);
+  //
+  //
+  //--------------------- logic for search bar 'searching'  --------------------------
+  const [locations, setLocations] = useState([1, 2, 3]);
+  const handleLocation = (loc) => {
+    console.log("location: ", loc);
+  };
+  //
+  //
   return (
-    <View>
-      <Text>weather</Text>
+    <View className="flex-1 relative">
+      <StatusBar style="light" />
+      <Image
+        blurRadius={70}
+        source={require("../../assets/images/weather/bg.png")}
+        className="absolute h-full w-full"
+      />
+      <SafeAreaView className="flex flex-1">
+        {/* ------------------------------------------- search section --------------------------------------- */}
+        <View style={{ height: "7%" }} className="mx-4 relative z-50">
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{
+              backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent",
+            }}
+          >
+            {showSearch ? (
+              <TextInput
+                placeholder="Search city"
+                placeholderTextColor={"lightgray"}
+                className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+              />
+            ) : null}
+
+            <TouchableOpacity
+              onPress={() => toggleSearch(!showSearch)}
+              style={{ backgroundColor: theme.bgWhite(0.3) }}
+              className="rounded-full p-3 m-1"
+            >
+              <MagnifyingGlassIcon size="25" color="white" />
+            </TouchableOpacity>
+          </View>
+
+          {/* ------------------------------------------- search section --------------------------------------- */}
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          {/* ------------------------------------------- search section functionality --------------------------------------- */}
+          {locations.length > 0 && showSearch ? (
+            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+              {locations.map((loc, index) => {
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? " border-b-2 border-b-gray-400"
+                  : "";
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => handleLocation(loc)}
+                    className={
+                      "flex-row items-center border-0 p-3 px-4 mb-1 " +
+                      borderClass
+                    }
+                  >
+                    <MapPinIcon size="20" color="gray" />
+                    <Text className="text-black text-lg ml-2">London</Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          ) : null}
+        </View>
+        {/* ------------------------------------------- search section functionality --------------------------------------- */}
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        {/* ------------------------------------------- forecast section --------------------------------------------------- */}
+      </SafeAreaView>
     </View>
   );
 };
