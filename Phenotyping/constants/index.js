43f99af0d8283a501c3fac68ccfef35df24ac01d
JaccobStanton <jstanton23@my.stlcc.edu>
2023-11-02 14:08:21 -0500

    weather app mostly complete

--- Phenotyping/constants/index.js
+++ Phenotyping/constants/index.js
@@ -4,6 +4,25 @@ import { NFTData } from "./dummy";
 import { CircleButton, RectButton } from "../components/Button";
 import { SubInfo } from "../components/SubInfo";
 
+export const apiKey = "1a94d707918b4cf4a22184039232803";
+export const weatherImages = {
+  "Partly cloudy": require("../assets/images/weather/partlycloudy.png"),
+  "Moderate rain": require("../assets/images/weather/moderaterain.png"),
+  "Patchy rain possible": require("../assets/images/weather/moderaterain.png"),
+  Sunny: require("../assets/images/weather/sun.png"),
+  Clear: require("../assets/images/weather/sun.png"),
+  Overcast: require("../assets/images/weather/cloud.png"),
+  Cloudy: require("../assets/images/weather/cloud.png"),
+  "Light rain": require("../assets/images/weather/moderaterain.png"),
+  "Moderate rain at times": require("../assets/images/weather/moderaterain.png"),
+  "Heavy rain": require("../assets/images/weather/heavyrain.png"),
+  "Heavy rain at times": require("../assets/images/weather/heavyrain.png"),
+  "Moderate or heavy freezing rain": require("../assets/images/weather/heavyrain.png"),
+  "Moderate or heavy rain shower": require("../assets/images/weather/heavyrain.png"),
+  "Moderate or heavy rain with thunder": require("../assets/images/weather/heavyrain.png"),
+  Mist: require("../assets/images/weather/mist.png"),
+  other: require("../assets/images/weather/moderaterain.png"),
+};
 export {
   assets,
   COLORS,
--- Phenotyping/screens/weather/WeatherLanding.js
+++ Phenotyping/screens/weather/WeatherLanding.js
@@ -8,243 +8,252 @@ import {
   ScrollView,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { theme } from "./theme";
 import {
   MagnifyingGlassIcon,
   CalendarDaysIcon,
+  XMarkIcon,
 } from "react-native-heroicons/outline";
 import { MapPinIcon } from "react-native-heroicons/solid";
+import { debounce } from "lodash";
+import { fetchLocations } from "../../api/weather";
+import { fetchWeatherForecast } from "../../api/weather";
+import { weatherImages } from "../../constants";
+import * as Progress from "react-native-progress";
+import { getData, storeData } from "../../utils/asyncStorage";
 
 const WeatherLanding = () => {
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState({});
+
   //--------------------- logic for search bar --------------------------
   const [showSearch, toggleSearch] = useState(false);
   //
   //
   //--------------------- logic for search bar 'searching'  --------------------------
-  const [locations, setLocations] = useState([1, 2, 3]);
+  const [locations, setLocations] = useState([]);
   const handleLocation = (loc) => {
-    console.log("location: ", loc);
+    // console.log("location: ", loc);
+    setLocations([]);
+    toggleSearch(false);
+    setLoading(true); //will show loading bar until app receives data
+    fetchWeatherForecast({
+      cityName: loc.name,
+      days: "7",
+    }).then((data) => {
+      setWeather(data); //this makes it so that when you click a location in the search bar, it saves it in state
+      setLoading(false); // will not show loading bar because you have the data
+      storeData("city", loc.name);
+      // console.log("got forecast: ", data);
+    });
   };
   //
   //
+
+  const handleSearch = (value) => {
+    // fetch locations
+    if (value.length > 2) {
+      fetchLocations({ cityName: value }).then((data) => {
+        setLocations(data);
+      });
+    }
+  };
+  // -----------async so that when you open this page, it has a preloaded values
+  useEffect(() => {
+    fetchMyWeatherData();
+  }, []);
+
+  const fetchMyWeatherData = async () => {
+    let myCity = await getData("city");
+    let cityName = "Islamabad";
+    if (myCity) {
+      cityName = myCity;
+    }
+    fetchWeatherForecast({
+      cityName,
+      days: "7",
+    }).then((data) => {
+      // console.log('got data: ',data.forecast.forecastday);
+      setWeather(data);
+      setLoading(false);
+    });
+    // --------------------------------------------------end of async code
+  };
+  const handleTextDebounce = useCallback(debounce(handleSearch, 1200), []); //the debounce is used so that the console does not return every character typed into the search bar, only what is typed in after 1.2 seconds
+  const { current, location } = weather;
+
   return (
     <View className="flex-1 relative">
       <StatusBar style="light" />
       <Image
         blurRadius={70}
         source={require("../../assets/images/weather/bg.png")}
-        className="absolute h-full w-full"
+        className="absolute w-full h-full"
       />
-      <SafeAreaView className="flex flex-1">
-        {/* ------------------------------------------- search section button --------------------------------------- */}
-        <View style={{ height: "7%" }} className="mx-4 relative z-50">
-          <View
-            className="flex-row justify-end items-center rounded-full"
-            style={{
-              backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent",
-            }}
-          >
-            {showSearch ? (
-              <TextInput
-                placeholder="Search city"
-                placeholderTextColor={"lightgray"}
-                className="pl-6 h-10 pb-1 flex-1 text-base text-white"
-              />
-            ) : null}
-
-            <TouchableOpacity
-              onPress={() => toggleSearch(!showSearch)}
-              style={{ backgroundColor: theme.bgWhite(0.3) }}
-              className="rounded-full p-3 m-1"
-            >
-              <MagnifyingGlassIcon size="25" color="white" />
-            </TouchableOpacity>
-          </View>
-
-          {/* ------------------------------------------- search section button --------------------------------------- */}
-          {/*  */}
-          {/*  */}
-          {/*  */}
-          {/* ------------------------------------------- search section functionality --------------------------------------- */}
-          {locations.length > 0 && showSearch ? (
-            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
-              {locations.map((loc, index) => {
-                let showBorder = index + 1 != locations.length;
-                let borderClass = showBorder
-                  ? " border-b-2 border-b-gray-400"
-                  : "";
-                return (
-                  <TouchableOpacity
-                    key={index}
-                    onPress={() => handleLocation(loc)}
-                    className={
-                      "flex-row items-center border-0 p-3 px-4 mb-1 " +
-                      borderClass
-                    }
-                  >
-                    <MapPinIcon size="20" color="gray" />
-                    <Text className="text-black text-lg ml-2">London</Text>
-                  </TouchableOpacity>
-                );
-              })}
-            </View>
-          ) : null}
+      {loading ? (
+        <View className="flex-1 flex-row justify-center items-center">
+          <Progress.CircleSnail thickness={10} size={140} color="#0bb3b2" />
         </View>
-        {/* ------------------------------------------- search section functionality end ------------------------------------ */}
-        {/*  */}
-        {/*  */}
-        {/*  */}
-        {/* ------------------------------------------- forecast section --------------------------------------------------- */}
-        <View className="mx-4 justify-around flex-1 mb-2">
-          {/* location */}
-          <Text className="text-white text-center text-2xl font-bold">
-            Saint Louis,
-            <Text className="text-lg font-semibold text-gray-300">
-              Missouri
-            </Text>
-          </Text>
-          {/* weather image */}
-          <View className="flex-row justify-center">
-            <Image
-              source={require("../../assets/images/weather/partlycloudy.png")}
-              className="w-52 h-52"
-            />
-          </View>
-          {/* degree celcius */}
-          <View className="space-y-2">
-            <Text className="text-center font-bold text-white text-6xl ml-5">
-              23&#176;
-            </Text>
-            <Text className="text-center text-white text-xl tracking-widest">
-              Partly cloudy
-            </Text>
-          </View>
-          {/* ------------------------------------------- forecast section end ------------------------------------------------ */}
-          {/*  */}
-          {/*  */}
-          {/*  */}
-          {/* ------------------------------------------------other stats-----------------------------*/}
-          <View className="flex-row justify-between mx-4">
-            <View className="flex-row space-x-2 items-center">
-              <Image
-                source={require("../../assets/images/weather/wind.png")}
-                className="h-6 w-6"
-              />
-              <Text className="text-white font-semibold text-base">22km</Text>
+      ) : (
+        <SafeAreaView className="flex flex-1">
+          {/* search section */}
+          <View style={{ height: "7%" }} className="mx-4 relative z-50">
+            <View
+              className="flex-row justify-end items-center rounded-full"
+              style={{
+                backgroundColor: showSearch
+                  ? theme.bgWhite(0.2)
+                  : "transparent",
+              }}
+            >
+              {showSearch ? (
+                <TextInput
+                  onChangeText={handleTextDebounce}
+                  placeholder="Search city"
+                  placeholderTextColor={"lightgray"}
+                  className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+                />
+              ) : null}
+              <TouchableOpacity
+                onPress={() => toggleSearch(!showSearch)}
+                className="rounded-full p-3 m-1"
+                style={{ backgroundColor: theme.bgWhite(0.3) }}
+              >
+                {showSearch ? (
+                  <XMarkIcon size="25" color="white" />
+                ) : (
+                  <MagnifyingGlassIcon size="25" color="white" />
+                )}
+              </TouchableOpacity>
             </View>
+            {locations.length > 0 && showSearch ? (
+              <View className="absolute w-full bg-gray-300 top-16 rounded-3xl ">
+                {locations.map((loc, index) => {
+                  let showBorder = index + 1 != locations.length;
+                  let borderClass = showBorder
+                    ? " border-b-2 border-b-gray-400"
+                    : "";
+                  return (
+                    <TouchableOpacity
+                      key={index}
+                      onPress={() => handleLocation(loc)}
+                      className={
+                        "flex-row items-center border-0 p-3 px-4 mb-1 " +
+                        borderClass
+                      }
+                    >
+                      <MapPinIcon size="20" color="gray" />
+                      <Text className="text-black text-lg ml-2">
+                        {loc?.name}, {loc?.country}
+                      </Text>
+                    </TouchableOpacity>
+                  );
+                })}
+              </View>
+            ) : null}
+          </View>
 
-            <View className="flex-row space-x-2 items-center">
+          {/* forecast section */}
+          <View className="mx-4 flex justify-around flex-1 mb-2">
+            {/* location */}
+            <Text className="text-white text-center text-2xl font-bold">
+              {location?.name},
+              <Text className="text-lg font-semibold text-gray-300">
+                {location?.country}
+              </Text>
+            </Text>
+            {/* weather icon */}
+            <View className="flex-row justify-center">
               <Image
-                source={require("../../assets/images/weather/drop.png")}
-                className="h-6 w-6"
+                // source={{uri: 'https:'+current?.condition?.icon}}
+                source={weatherImages[current?.condition?.text || "other"]}
+                className="w-52 h-52"
               />
-              <Text className="text-white font-semibold text-base">22%</Text>
             </View>
-
-            <View className="flex-row space-x-2 items-center">
-              <Image
-                source={require("../../assets/images/weather/sun.png")}
-                className="h-6 w-6"
-              />
-              <Text className="text-white font-semibold text-base">
-                6:05 AM
+            {/* degree celcius */}
+            <View className="space-y-2">
+              <Text className="text-center font-bold text-white text-6xl ml-5">
+                {current?.temp_c}&#176;
+              </Text>
+              <Text className="text-center text-white text-xl tracking-widest">
+                {current?.condition?.text}
               </Text>
             </View>
-          </View>
-        </View>
 
-        {/* ------------------------------------------------other stats end--------------------------*/}
-        {/*  */}
-        {/*  */}
-        {/*  */}
-        {/* ----------------------------------------------forecast for next days ------------------ */}
-        <View className="mb-2 space-y-2">
-          <View className="flex-row items-center mx-5 space-x-2">
-            <CalendarDaysIcon size="22" color="white" />
-            <Text className="text-white text-base"> Daily forecast</Text>
+            {/* other stats */}
+            <View className="flex-row justify-between mx-4">
+              <View className="flex-row space-x-2 items-center">
+                <Image
+                  source={require("../../assets/images/weather/wind.png")}
+                  className="w-6 h-6"
+                />
+                <Text className="text-white font-semibold text-base">
+                  {current?.wind_kph}km
+                </Text>
+              </View>
+              <View className="flex-row space-x-2 items-center">
+                <Image
+                  source={require("../../assets/images/weather/drop.png")}
+                  className="w-6 h-6"
+                />
+                <Text className="text-white font-semibold text-base">
+                  {current?.humidity}%
+                </Text>
+              </View>
+              <View className="flex-row space-x-2 items-center">
+                <Image
+                  source={require("../../assets/images/weather/sun.png")}
+                  className="w-6 h-6"
+                />
+                <Text className="text-white font-semibold text-base">
+                  {weather?.forecast?.forecastday[0]?.astro?.sunrise}
+                </Text>
+              </View>
+            </View>
           </View>
-          <ScrollView
-            horizontal
-            contentContainerStyle={{ paddingHorizontal: 15 }}
-            showsHorizontalScrollIndicator={false}
-          >
-            {/* Monday */}
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require("../../assets/images/weather/heavyrain.png")}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Monday</Text>
-              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+
+          {/* forecast for next days */}
+          <View className="mb-2 space-y-3">
+            <View className="flex-row items-center mx-5 space-x-2">
+              <CalendarDaysIcon size="22" color="white" />
+              <Text className="text-white text-base">Daily forecast</Text>
             </View>
-            {/* Monday */}
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            <ScrollView
+              horizontal
+              contentContainerStyle={{ paddingHorizontal: 15 }}
+              showsHorizontalScrollIndicator={false}
             >
-              <Image
-                source={require("../../assets/images/weather/heavyrain.png")}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Monday</Text>
-              <Text className="text-white text-xl font-semibold">13&#176;</Text>
-            </View>
-            {/* Monday */}
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require("../../assets/images/weather/heavyrain.png")}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Monday</Text>
-              <Text className="text-white text-xl font-semibold">13&#176;</Text>
-            </View>
-            {/* Monday */}
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require("../../assets/images/weather/heavyrain.png")}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Monday</Text>
-              <Text className="text-white text-xl font-semibold">13&#176;</Text>
-            </View>
-            {/* Monday */}
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require("../../assets/images/weather/heavyrain.png")}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Monday</Text>
-              <Text className="text-white text-xl font-semibold">13&#176;</Text>
-            </View>
-            {/* Monday */}
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require("../../assets/images/weather/heavyrain.png")}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Monday</Text>
-              <Text className="text-white text-xl font-semibold">13&#176;</Text>
-            </View>
-          </ScrollView>
-        </View>
-        {/* ----------------------------------------------forecast for next days end --------------- */}
-      </SafeAreaView>
+              {weather?.forecast?.forecastday?.map((item, index) => {
+                const date = new Date(item.date);
+                const options = { weekday: "long" };
+                let dayName = date.toLocaleDateString("en-US", options);
+                dayName = dayName.split(",")[0];
+
+                return (
+                  <View
+                    key={index}
+                    className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+                    style={{ backgroundColor: theme.bgWhite(0.15) }}
+                  >
+                    <Image
+                      // source={{uri: 'https:'+item?.day?.condition?.icon}}
+                      source={
+                        weatherImages[item?.day?.condition?.text || "other"]
+                      }
+                      className="w-11 h-11"
+                    />
+                    <Text className="text-white">{dayName}</Text>
+                    <Text className="text-white text-xl font-semibold">
+                      {item?.day?.avgtemp_c}&#176;
+                    </Text>
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </View>
+        </SafeAreaView>
+      )}
     </View>
   );
 };
