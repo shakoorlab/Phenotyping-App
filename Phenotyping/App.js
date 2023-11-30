aa2c2c733dad76bea5f503c36029930261a82f49
JaccobStanton <jstanton23@my.stlcc.edu>
2023-11-30 12:08:17 -0600

    added code for customer drawer popout

--- Phenotyping/App.js
+++ Phenotyping/App.js
@@ -1,18 +1,64 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator, DrawerItem } from "@react-navigation/drawer";
+import { useFonts } from "expo-font";
 import LoginScreen from "./screens/login/LoginScreen";
 import SignupScreen from "./screens/signup/SignupScreen";
 import OnboardingScreen from "./screens/onboarding/OnboardingScreen";
 import FieldSelectionScreen from "./screens/fieldSelection/FieldSelection";
 import Details from "./screens/FieldDetails/Details";
-import { useFonts } from "expo-font";
 import WeatherLanding from "./screens/weather/WeatherLanding";
 import PlotRowSelection from "./screens/plotRow/PlotRowSelection";
 import QuestionsScreen from "./screens/questionaire/QuestionsScreen";
 import MeasurementSelectionScreen from "./screens/MeasurementSelection/MeasurementSelectionScreen";
+import { BlurView } from "expo-blur"; // Changed from @react-native-community/blur
+import { View, Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const CustomDrawerContent = (props) => {
+  return (
+    <BlurView intensity={50} style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 20 }}>
+        <Text>User 1</Text>
+        <DrawerItem
+          label="Home"
+          onPress={() => props.navigation.navigate("Home")}
+        />
+        <DrawerItem
+          label="Field Selection"
+          onPress={() => props.navigation.navigate("FieldSelection")}
+        />
+
+        <DrawerItem label="Sign Out" />
+      </View>
+    </BlurView>
+  );
+};
+
+function MainStackNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="OnboardingScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignupScreen} />
+      <Stack.Screen name="FieldSelection" component={FieldSelectionScreen} />
+      <Stack.Screen name="Details" component={Details} />
+      <Stack.Screen name="Weather" component={WeatherLanding} />
+      <Stack.Screen
+        name="MeasurementSelectionScreen"
+        component={MeasurementSelectionScreen}
+      />
+      <Stack.Screen name="PlotRowSelection" component={PlotRowSelection} />
+      <Stack.Screen name="QuestionsScreen" component={QuestionsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   const [loaded] = useFonts({
@@ -27,24 +73,18 @@ function App() {
 
   return (
     <NavigationContainer>
-      {/* below sets what page app opens on, and removes the header */}
-      <Stack.Navigator
-        initialRouteName="OnboardingScreen"
-        screenOptions={{ headerShown: false }}
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{ drawerType: "slide", drawerStyle: { width: "60%" } }}
       >
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignupScreen} />
-        <Stack.Screen name="FieldSelection" component={FieldSelectionScreen} />
-        <Stack.Screen name="Details" component={Details} />
-        <Stack.Screen name="Weather" component={WeatherLanding} />
-        <Stack.Screen
-          name="MeasurementSelectionScreen"
-          component={MeasurementSelectionScreen}
+        {/* The name 'MainStack' can be any name you choose */}
+        <Drawer.Screen
+          name="MainStack"
+          component={MainStackNavigator}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen name="PlotRowSelection" component={PlotRowSelection} />
-        <Stack.Screen name="QuestionsScreen" component={QuestionsScreen} />
-      </Stack.Navigator>
+        {/* Add more drawer screens if needed */}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
