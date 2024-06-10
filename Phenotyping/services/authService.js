0a7ec9a3e4a6023577b7cdda33f3b717dd09152b
JaccobStanton <jstanton23@my.stlcc.edu>
2024-06-10 15:05:18 -0500

    Better file structuring of the project

---
+++ Phenotyping/services/authService.js
@@ -0,0 +1,16 @@
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export const login = async (email, password) => {
+  const auth = FIREBASE_AUTH;
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
