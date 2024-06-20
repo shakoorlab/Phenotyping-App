5aaeb9d362d7eb134c03d416ae5c73b7c0df9e8d
JaccobStanton <jstanton23@my.stlcc.edu>
2024-06-20 11:06:48 -0500

    Made change to import statement

--- Phenotyping/frontend/navigation/MainStackNavigator.js
+++ Phenotyping/frontend/navigation/MainStackNavigator.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import FieldSelectionScreen from "../screens/field_selection/FieldSelection";
 import Details from "../screens/field_details/Details";
 import MeasurementSelectionScreen from "../screens/measurement_selection/MeasurementSelectionScreen";
-import MapScreen from "../screens/mapview/MapScreen";
+import MapScreen from "../screens/map_view/MapScreen";
 import QuestionsScreen from "../screens/questionaire/QuestionsScreen";
 
 const Stack = createNativeStackNavigator();
