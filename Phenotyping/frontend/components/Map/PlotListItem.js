ea3053a328f48ae43a485aec38aba7b59489e1e2
JaccobStanton <jstanton23@my.stlcc.edu>
2024-06-20 11:04:11 -0500

    Added linear gradient from expo package

--- Phenotyping/frontend/components/Map/PlotListItem.js
+++ Phenotyping/frontend/components/Map/PlotListItem.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
-import Animated, { FadeInDown, FadeInUp } from "react-native-reanimated";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
 
 const PlotListItem = ({ plot, containerStyle }) => {
   return (
@@ -8,8 +9,16 @@ const PlotListItem = ({ plot, containerStyle }) => {
       style={[styles.card, containerStyle]}
       entering={FadeInDown.duration(1000).springify()}
     >
-      <Image source={{ uri: plot.image }} style={styles.image} />
-      <View style={styles.rightContainer}>
+      <Image
+        source={require("../../assets/images/sorghum.png")}
+        style={styles.image}
+      />
+      <LinearGradient
+        colors={["#8cc751", "#32de84"]} //#FFD439 #FF7A00
+        start={[0, 0]}
+        end={[3, 1]}
+        style={styles.rightContainer}
+      >
         <Text style={styles.title}>{plot.plotNumber}</Text>
         <Text style={styles.description}>
           This plot is part of the Climate Smart project
@@ -19,35 +28,38 @@ const PlotListItem = ({ plot, containerStyle }) => {
           <Text style={styles.price}>Field {plot.field}</Text>
           <Text style={styles.price}>PI {plot.piNumber}</Text>
         </View>
-      </View>
+      </LinearGradient>
     </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
+    marginBottom: 5,
     borderRadius: 20,
     flexDirection: "row",
-    overflow: "hidden", //allows for the card border radius to be used on the image, even if the image is "above" the card
+    overflow: "hidden", // allows for the card border radius to be used on the image, even if the image is "above" the card
     borderWidth: 1,
     borderColor: "black",
   },
 
   image: {
     width: 150,
-    aspectRatio: 1, //width and height equal
+    height: 150,
+    aspectRatio: 1, // width and height equal
+    backgroundColor: "lightblue",
+    borderRightWidth: 0.5,
   },
   rightContainer: {
     padding: 10,
-    flex: 1, //take all available space, but nothing more
-    backgroundColor: "#FFFFFF",
+    flex: 1, // take all available space, but nothing more
   },
   title: {
     fontFamily: "InterBold",
     marginBottom: 10,
     fontSize: 16,
   },
-  description: { color: "gray" },
+  description: { color: "#181818", fontSize: 16, fontFamily: "InterMedium" },
   price: { fontFamily: "InterBold" },
 
   footer: {
