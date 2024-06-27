4a96bc82ccb91aee7b3bb56b6f69f297ec6e4d55
JaccobStanton <jstanton23@my.stlcc.edu>
2024-06-27 13:43:51 -0500

    Added temporary person photos and moved assets.js to assets folder

---
+++ Phenotyping/frontend/assets/assets.js
@@ -0,0 +1,33 @@
+import badge from "./icons/badge.png";
+import clock from "./icons/clock.png";
+import heart from "./icons/heart.png";
+import unchecked_heart from "./icons/unchecked_heart.png";
+import left from "./icons/left.png";
+import search from "./icons/search.png";
+import storm from "./icons/storm.png";
+import person02 from "./icons/person02.png";
+import person03 from "./icons/person03.png";
+import person04 from "./icons/person04.png";
+
+import logo from "./images/DDPSC_Logo.png";
+import nft01 from "./images/mo.png";
+import nft02 from "./images/sc.png";
+import nft03 from "./images/smart.png";
+
+export default {
+  badge,
+  clock,
+  heart,
+  unchecked_heart,
+  left,
+  search,
+  storm,
+  person02,
+  person03,
+  person04,
+
+  logo,
+  nft01,
+  nft02,
+  nft03,
+};
