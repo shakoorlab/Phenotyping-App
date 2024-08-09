d960876d19080ede154be6ee2d6530770c477515
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-09 10:34:46 -0500

    Added and removed images for app

--- Phenotyping/frontend/assets/assets.js
+++ Phenotyping/frontend/assets/assets.js
@@ -2,7 +2,8 @@ import badge from "./icons/badge.png";
 import clock from "./icons/clock.png";
 import heart from "./icons/heart.png";
 import unchecked_heart from "./icons/unchecked_heart.png";
-import left from "./icons/left.png";
+import hamburgerMenu from "./icons/hamburger-menu.png";
+import left from "./icons/left-arrow-50.png";
 import search from "./icons/search.png";
 import storm from "./icons/storm.png";
 import person02 from "./icons/person02.png";
@@ -10,15 +11,13 @@ import person03 from "./icons/person03.png";
 import person04 from "./icons/person04.png";
 
 import logo from "./images/DDPSC_Logo.png";
-import nft01 from "./images/mo.png";
-import nft02 from "./images/sc.png";
-import nft03 from "./images/smart.png";
 
 export default {
   badge,
   clock,
   heart,
   unchecked_heart,
+  hamburgerMenu,
   left,
   search,
   storm,
@@ -27,7 +26,4 @@ export default {
   person04,
 
   logo,
-  nft01,
-  nft02,
-  nft03,
 };
